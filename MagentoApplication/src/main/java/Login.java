460734f7722ca894b0317e4fc525a3f6872d1f98
userr <userr@user>
2018-08-28 14:47:09 +0530

    first comment

---
+++ MagentoApplication/src/main/java/Login.java
@@ -0,0 +1,35 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Login {
+  
+	
+	
+	
+	
+	
+	
+	
+	WebDriver driver;
+    By email = By.id("email");
+    By pwd = By.id("pass");
+    By log = By.id("send2");
+	public Login(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	public void typeEmail(String data)
+	{
+		driver.findElement(email).sendKeys(data);
+	}
+	public void typePassWord(String data)
+	{
+		driver.findElement(pwd).sendKeys(data);
+	}
+	public void clickLogin()
+	{
+		driver.findElement(log).click();
+	}
+}
